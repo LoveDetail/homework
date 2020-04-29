063decffa84be9f185d0f99a7005a49957f4e870
LoveDetail <sea_1123@aliyun.com>
2020-04-29 10:19:32 +0800

    +mapper 注解

---
+++ src/main/java/com/gp/homework/common/GetField.java
@@ -0,0 +1,140 @@
+package com.gp.homework.common;
+
+
+import lombok.Cleanup;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
+@SuppressWarnings(value="unused")
+public class GetField {
+	
+	public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException {
+
+		String username = "root" ;
+		String password = "abc123" ;
+
+		//获取数据表的字段，用来生成DTO的变量 
+		getDateBaseField(JdbcUrlEnum.MYSQLDRIVER8,username,password,"SELECT * FROM materiel LIMIT 0") ;
+//		getDateBaseField(JdbcUrlEnum.SQLSERVER_MICROSOFT,username,password,"select top 0 * from test") ;
+		
+		//生成insert语句
+//		String sql = "SELECT a.omocd stcd,a.omonm stnm,b.sttp,'0' ptype,b.rvnm,b.hnnm,b.bsnm,a.lgtd,a.lttd,a.address stlc,'13000002' sunit,'130000' addvcd,'1900-01-01' esstym,'' pnumber,IFNULL(b.FRGRD,'0') FRGRD ,'' comments,'1900-01-01' optm,'' sub_create,'heBeiService02' sub_serviceId,a.omoid sub_omoid,a.stid sub_stid,'0' sub_transType,'http://219.148.51.67:8455' sub_host FROM ioms_omo_b a INNER JOIN ioms_stbprp_b b ON a.omoid = b.omoid" ;
+//		sql2Insert(JdbcUrlEnum.MYSQLDRIVER8, username,password,"ioms_stbprp_b.sql","ioms_stbprp_b", "SELECT '19841123' omoid,STCD,STTP,FRGRD,RVNM,BSNM,HNNM,BGFRYM,ESSTYM,STAZT,STATUS,OPERATOR FROM ioms_stbprp_b") ;
+
+	}
+	
+	private static void getDateBaseField(JdbcUrlEnum E,String username,String password,String sql)throws ClassNotFoundException, SQLException{
+
+
+		Class.forName(E.DRIVER) ;
+		@Cleanup ResultSet rs = DriverManager.getConnection(E.URL,username,password).createStatement().executeQuery(sql) ;
+			
+		for(int i=1; i<= rs.getMetaData().getColumnCount();i++)
+			System.out.println("private "+transLateField(rs.getMetaData().getColumnTypeName(i))+" "+rs.getMetaData().getColumnName(i).toLowerCase()+" ;");
+		
+
+	}
+	
+	private static void sql2Insert(JdbcUrlEnum E,String username,String password,String fileName,String tableName,String sql) throws ClassNotFoundException, SQLException, IOException{
+		
+		Class.forName(E.DRIVER) ;
+		
+		@Cleanup Connection conn = DriverManager.getConnection(E.URL, username,password) ;
+		@Cleanup Statement sa = conn.createStatement() ;
+		@Cleanup ResultSet rs = sa.executeQuery(sql) ;
+		
+		
+		StringBuilder insertStr = new StringBuilder() ;
+		StringBuilder valueStr = new StringBuilder() ;
+		
+		while(rs.next()) {
+			insertStr.append("insert into ").append(tableName).append(" (") ;
+			valueStr.append(" values (") ;
+			for(int i=1; i<=rs.getMetaData().getColumnCount(); i++){
+				insertStr.append(rs.getMetaData().getColumnLabel(i)) ;
+				valueStr.append("'").append(rs.getObject(i)).append("'") ;
+				
+				if(i != rs.getMetaData().getColumnCount()){
+					insertStr.append(",") ;
+					valueStr.append(",") ;
+				}
+				else{
+					insertStr.append(")") ;
+					valueStr.append(") ;") ;
+				}
+			}
+			String s = insertStr.append(valueStr).toString() ;
+			System.out.println(s);
+			wrightTxt(s,fileName) ;
+			insertStr.setLength(0) ;
+			valueStr.setLength(0) ;
+		}
+
+	}
+	
+	private static String transLateField(String fieldType){
+//		System.out.println("***********"+fieldType);
+
+		if( "varchar".equalsIgnoreCase(fieldType) || "char".equalsIgnoreCase(fieldType) || "nvarchar".equalsIgnoreCase(fieldType) ||"nchar".equalsIgnoreCase(fieldType))
+			return "String" ;
+		else 
+			if(Objects.equals("TIMESTAMP",fieldType) || "datetime".equalsIgnoreCase(fieldType))
+			return "Date" ;
+		else 
+			if("TINYINT".equalsIgnoreCase(fieldType) || "bigint".equalsIgnoreCase(fieldType) || "int".equalsIgnoreCase(fieldType) || "integer".equalsIgnoreCase(fieldType))
+			return "Integer" ;
+		else 
+			if("decimal".equalsIgnoreCase(fieldType) || "float".equalsIgnoreCase(fieldType) || "money".equalsIgnoreCase(fieldType) || "numeric".equalsIgnoreCase(fieldType))
+			return "BigDecimal" ;
+		
+		
+		
+		return "AAAAAAAAAAAAAAAAAAA" ;
+	}
+	
+	
+	private static void wrightTxt(String s,String fileName) throws IOException{
+
+		@Cleanup FileWriter fileWriter=new FileWriter("D:\\"+fileName,true);
+		   int [] a=new int[]{11112,222,333,444,555,666};
+		   
+//		   for(String s : sqls){
+			   fileWriter.write(s+" \r\n");
+//		   }
+		   
+//		   for (int i = 0; i < a.length; i++) {
+//		    fileWriter.write(String.valueOf(a[i])+" ");
+//		     }
+		   fileWriter.flush();
+	}
+	
+
+	@SuppressWarnings(value="unused")
+	private enum JdbcUrlEnum{
+		MYSQLDRIVER5("jdbc:mysql://localhost:3306/bstemp?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true","com.mysql.jdbc.Driver"),
+		MYSQLDRIVER8("jdbc:mysql://localhost:3306/bstemp?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8","com.mysql.cj.jdbc.Driver"),
+
+		SQLSERVER_MICROSOFT("jdbc:sqlserver://192.168.2.82:1433;DatabaseName=YC5","com.microsoft.sqlserver.jdbc.SQLServerDriver"),
+		SQLSERVER_JTDS("jdbc:jtds://192.168.2.82:1433;DatabaseName=YC5","net.sourceforge.jtds.jdbc.Driver")
+
+		;
+
+		public String URL ;
+		public String DRIVER;
+
+		JdbcUrlEnum(String URL, String DRIVER) {
+			this.URL = URL;
+			this.DRIVER = DRIVER;
+		}
+	}
+	
+	
+	
+}
