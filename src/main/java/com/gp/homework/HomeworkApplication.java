388512389e260c1dee18c98508ab14c299096d89
LoveDetail <sea_1123@aliyun.com>
2020-04-29 10:16:27 +0800

    +mapper 注解

--- src/main/java/com/gp/homework/HomeworkApplication.java
+++ src/main/java/com/gp/homework/HomeworkApplication.java
@@ -1,9 +1,11 @@
 package com.gp.homework;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.gp.homework.domain.mapper")
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
