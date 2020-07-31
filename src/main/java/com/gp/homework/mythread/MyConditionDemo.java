7f626af70c598f63baf516f2fe8bddcac4c43fd0
LoveDetail <sea_1123@aliyun.com>
2020-07-31 16:30:04 +0800

    License

---
+++ src/main/java/com/gp/homework/mythread/MyConditionDemo.java
@@ -0,0 +1,74 @@
+package com.gp.homework.mythread;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Create by Wayne on 2020/7/31
+ */
+public class MyConditionDemo {
+
+    private static ReentrantLock lock = new ReentrantLock();
+    private static int number ;
+
+
+    public static void main(String[] args) {
+        OddEvenNumber number = new OddEvenNumber() ;
+        new Thread(()-> number.odd(),"Thread-odd").start();
+        new Thread(()-> number.even(),"Thread-even").start();
+    }
+
+
+    private static boolean isOdd(int number){
+        return (number & 1) == 1;
+    }
+
+    static class OddEvenNumber{
+
+        static Condition condition = lock.newCondition();
+
+        @SneakyThrows
+        public static void odd(){
+            for(;;){
+            lock.lock();
+                try{
+                    if(isOdd(number)){
+                        System.out.println(StrUtil.format("threadName::::{} =====>{}", Thread.currentThread().getName(), number++));
+                        TimeUnit.SECONDS.sleep(1);
+                        condition.signal();
+                    }
+                        condition.await() ;
+                }
+                finally {
+                    lock.unlock();
+                }
+            }
+        }
+
+
+        @SneakyThrows
+        public static void even(){
+
+            for(;;){
+               try{
+                   lock.lock();
+                   if(!isOdd(number)){
+                       System.out.println(StrUtil.format("threadName::::{} =====>{}", Thread.currentThread().getName(), number++));
+                       TimeUnit.SECONDS.sleep(1);
+                       condition.signal();
+                   }
+                       condition.await();
+               }
+               finally {
+                   lock.unlock();
+               }
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/com/gp/homework/mythread/MyNotifyDemo.java
@@ -0,0 +1,78 @@
+package com.gp.homework.mythread;
+
+import cn.hutool.core.util.StrUtil;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Create by Wayne on 2020/7/31
+ * 两个现成轮流打印奇数和偶数
+ *
+ */
+public class MyNotifyDemo {
+
+    private static int number = 1 ;
+    private static boolean isOdd = true ;
+
+
+    public static void main(String[] args) {
+        OddEventNumber o = new OddEventNumber() ;
+
+        new Thread(o :: odd).start();
+        new Thread(o :: even).start();
+    }
+
+   static class OddEventNumber {
+
+
+        public synchronized void odd()  {
+
+            while (true){
+                if(isOdd){
+                    System.out.println(StrUtil.format("threadName::::{} ===> {}",Thread.currentThread().getName(),number++));
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    isOdd = false ;
+                    notify() ;
+                }
+                else{
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        public synchronized void even() {
+            while (true){
+                if(!isOdd){
+                    System.out.println(StrUtil.format("threadName::::{} ===> {}",Thread.currentThread().getName(),number++));
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    isOdd = true ;
+                    notify() ;
+                }
+                else{
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
+
+
