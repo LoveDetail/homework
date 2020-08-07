de67742207ca3e5301f89886cd29058eb7011aaf
LoveDetail <sea_1123@aliyun.com>
2020-08-07 15:58:33 +0800

    分布式锁注解

---
+++ src/main/java/com/common/annotation/DistributionLockLisenor.java
@@ -0,0 +1,26 @@
+package com.common.annotation;
+
+import java.lang.annotation.*;
+
+
+/**
+ * @功能描述 防止重复提交标记注解
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface DistributionLockLisenor {
+
+    /**
+     * 锁的有效时间，单位为秒，默认值为2
+     * @return
+     */
+    int timeInSecond() default 2;
+
+    /**
+     * 需要根据分布式锁执行的业务类型
+     * @return
+     */
+    String functionName() default "" ;
+
+}
